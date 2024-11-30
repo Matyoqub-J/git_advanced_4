dce2bc6ec62a4bf521d0c0566eeef50d14781a32
matyoqub <m.jumanazarov@newuu.uz>
2024-12-01 00:12:52 +0900

    Fixed the processing of files in train_file_list_to_json

--- main.py
+++ main.py
@@ -15,15 +15,29 @@ def path_to_file_list(path: str) -> List[str]:
 
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
-    """Converts two lists of file paths into a list of json strings"""
-    # Preprocess unwanted characters
+    """Converts two lists of file paths into a list of JSON strings."""
+    if len(english_file_list) != len(german_file_list):
+        print("Error: English and German file lists have different lengths.")
+        return []
+
     def process_file(file):
         if '\\' in file:
             file = file.replace('\\', '\\\\')
-        if '/' or '"' in file:
+        if '/' in file or '"' in file:
             file = file.replace('/', '\\/')
             file = file.replace('"', '\\"')
         return file
+
+    template_start = '{\"English\":\"'
+    template_mid = '\",\"German\":\"'
+    template_end = '\"}'
+
+    processed_file_list = []
+    for english_file, german_file in zip(english_file_list, german_file_list):
+        english_file = process_file(english_file)
+        german_file = process_file(german_file)
+        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
+    return processed_file_list
 
     # Template for json file
     template_start = '{\"English\":\"'
